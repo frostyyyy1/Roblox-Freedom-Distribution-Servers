be2b83c41c5ff75ca19914f8c20182b4d7fe0a87
frost <88431649+frostyyyy1@users.noreply.github.com>
2026-02-01 15:03:53 +0100

    Update check.py

--- check.py
+++ check.py
@@ -7,6 +7,12 @@ socket.setdefaulttimeout(5)
 online = []
 offline = []
 
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
 with open("ips.list") as f:
     for line in f:
         line = line.strip()
@@ -18,12 +24,12 @@ with open("ips.list") as f:
             url = line
         try:
             req = urllib.request.Request(url, method="HEAD")
-            urllib.request.urlopen(req)
+            urllib.request.urlopen(req, context=ctx)
             online.append(line)
         except Exception:
             try:
                 req = urllib.request.Request(url, method="GET")
-                urllib.request.urlopen(req)
+                urllib.request.urlopen(req, context=ctx)
                 online.append(line)
             except Exception:
                 offline.append(line)
